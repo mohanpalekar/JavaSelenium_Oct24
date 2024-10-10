d917470a7f3680ea97f9c42aed3bab3702ad650b
palek <palek@Nikita>
2024-10-10 20:26:41 +0530

    Add code for Obejcts and Wrapper classes

---
+++ JavaPractice/src/main/java/DataTypes/ObjectsInJava.java
@@ -0,0 +1,23 @@
+package DataTypes;
+
+public class ObjectsInJava {
+	
+	public static void main(String[] args) {
+		
+		String str = new String("Hello World");
+		
+		System.out.println(str);
+		
+		int [] arr = new int[5];
+		
+		System.out.println(str.length());
+		
+		System.out.println(str.charAt(2));
+		
+		System.out.println(str.contains("O"));
+		
+		
+		
+	}
+
+}
---
+++ JavaPractice/src/main/java/DataTypes/WrapperClass.java
@@ -0,0 +1,34 @@
+package DataTypes;
+
+public class WrapperClass {
+	
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		System.out.println(i);
+		
+		Integer j = 123;
+		
+		System.out.println(j);
+		
+		Short s = 12;
+		
+		Byte b = 23;
+		
+		Long L = 13122371313l;
+		
+		System.out.println(L);
+		
+		Boolean B = true;
+		
+		Character ch = 'c';
+		
+		String str = "Hey There";
+		
+		System.out.println(str.toUpperCase());
+		
+		
+	}
+
+}
