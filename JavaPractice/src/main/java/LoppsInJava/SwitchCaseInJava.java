56c831e50895bc72173492b0c83737109808694e
palek <palek@192.168.1.34>
2024-10-16 20:33:16 +0530

    Switch case in Java

--- JavaPractice/src/main/java/LoppsInJava/SwitchCaseInJava.java
+++ JavaPractice/src/main/java/LoppsInJava/SwitchCaseInJava.java
@@ -2,4 +2,32 @@ package LoppsInJava;
 
 public class SwitchCaseInJava {
 
+	public static void main(String[] args) {
+
+
+		int i = 0;
+
+		switch(i) {
+
+		case 1:
+			System.out.println("One");
+			break;
+
+		case 2:
+			System.out.println("Two");
+			break;
+
+		case 3:
+			System.out.println("Three");
+			break;
+
+		default:
+			System.out.println("0 or greatter than 3");
+			
+		}
+		
+		
+
+	}
+
 }
